90be38e7bef56075064b45a93927ab594df59470
Serif Çolakel <serifcolakel0@gmail.com>
2023-03-25 00:03:05 +0300

    UPDATED (serif) : CustomButton.tsx desing updated.

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,9 +1,45 @@
-import {TouchableOpacity, StyleSheet, StyleProp, ViewStyle} from 'react-native';
+import {TouchableOpacity, StyleSheet} from 'react-native';
 import React from 'react';
 import CustomText from './CustomText';
 import {colors} from '../utils/colors';
-import {isAndroid} from '../utils/platformUtil';
-import {FONT_FAMILY} from '../constant';
+import styled from 'styled-components';
+
+export interface IconProps {
+  height?: number;
+  type?: 'outlined' | 'contained';
+  color?: string;
+}
+
+const StyleButton = styled(TouchableOpacity)<IconProps>`
+  width: 100%;
+  margin-vertical: 5px;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  border-bottom-left-radius: 0px;
+  border-bottom-right-radius: 10px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 0px;
+  height: ${({height}) => height}px;
+  ${props => {
+    switch (props.type) {
+      case 'outlined':
+        return `
+          border-width: 1px;
+          border-color: ${props.color};
+          background-color: transparent;
+        `;
+      case 'contained':
+        return `
+          background-color: ${props.color};
+        `;
+      default:
+        return `
+          background-color: ${props.color};
+        `;
+    }
+  }}
+`;
 
 type Props = {
   onPress: () => void;
@@ -11,7 +47,6 @@ type Props = {
   color?: string;
   disabled?: boolean;
   type?: 'outlined' | 'contained';
-  extraStyle?: StyleProp<ViewStyle>;
   height?: number;
 };
 
@@ -21,55 +56,36 @@ const CustomButton = ({
   color = colors.primary,
   disabled = false,
   type = 'contained',
-  extraStyle = {},
   height = 44,
 }: Props) => {
   return (
-    <TouchableOpacity
-      disabled={disabled}
-      activeOpacity={0.8}
-      onPress={onPress}
-      style={[
-        {
-          ...styles.button,
-          height: height,
-          borderColor: disabled ? colors.gray2 : color,
-          backgroundColor: disabled
-            ? colors.white
-            : type === 'outlined'
-            ? 'transparent'
-            : color,
-        },
-        extraStyle,
-      ]}>
-      <CustomText
-        label={label}
-        style={{
-          ...styles.buttonText,
-          color: disabled
-            ? colors.primary
-            : type === 'outlined'
-            ? colors.primary
-            : colors.white,
-          fontFamily: isAndroid ? FONT_FAMILY.REGULAR : undefined,
-        }}
-      />
-    </TouchableOpacity>
+    <>
+      <StyleButton
+        color={color}
+        disabled={disabled}
+        activeOpacity={0.8}
+        type={type}
+        height={height}
+        onPress={onPress}>
+        <CustomText
+          label={label}
+          style={{
+            ...styles.buttonText,
+            color: disabled
+              ? colors.primary
+              : type === 'outlined'
+              ? colors.primary
+              : colors.white,
+          }}
+        />
+      </StyleButton>
+    </>
   );
 };
 
 export default CustomButton;
 
 const styles = StyleSheet.create({
-  button: {
-    padding: 6,
-    width: '100%',
-    borderWidth: 1,
-    borderRadius: 25,
-    marginVertical: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   buttonText: {
     fontSize: 16,
   },
