d2da94b360e5985d4350d131223c91f98041d951
Serif Çolakel <serifcolakel0@gmail.com>
2023-03-25 00:33:57 +0300

    UPDATED (serif) : Details.tsx screen updated with styled component & react-hook-form.

--- src/screens/details/Details.tsx
+++ src/screens/details/Details.tsx
@@ -8,163 +8,9 @@ import CustomText from '../../components/CustomText';
 import CustomButton from '../../components/CustomButton';
 import {TabView, SceneMap, TabBar} from 'react-native-tab-view';
 import {colors} from '../../utils/colors';
-import {isAndroid} from '../../utils/platformUtil';
-import {FONT_FAMILY, PADDING} from '../../constant';
+import {FONT_SIZE, PADDING} from '../../constant';
 import styled from 'styled-components';
-
-const DetailsTitle = styled(CustomText)`
-  font-size: 15px;
-  margin: 10px 0;
-  border-bottom-width: 1px;
-  border-bottom-color: ${colors.gray};
-  color: ${colors.scarlet};
-  padding-bottom: 10px;
-`;
-
-type DetailsProps = NativeStackScreenProps<RootStackParamList, 'Details'>;
-
-type FormValues = {
-  username: string;
-  password: string;
-};
-
-const FirstRoute = ({navigation}: DetailsProps) => {
-  const handleGoToHome = () => navigation.navigate('Home');
-  const {control, handleSubmit} = useForm<FormValues>();
-  const onSubmit: SubmitHandler<FormValues> = data => console.warn(data);
-  return (
-    <View style={styles.routeContainer}>
-      <CustomText label={'Firs Screen is about to be implemented'} />
-      <DetailsTitle
-        label={'Below is a form with react-hook-form and styled-components'}
-      />
-      <View style={styles.inputContainer}>
-        <CustomInput
-          label="Kullanıcı Adı"
-          control={control}
-          name="username"
-          placeholder="Kullanıcı Adı"
-          rules={{required: 'Kullanıcı Adı zorunludur'}}
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <CustomInput
-          label="Şifre"
-          control={control}
-          name="password"
-          placeholder="Şifre"
-          rules={{
-            required: 'Şifre zorunludur',
-            minLength: {
-              value: 3,
-              message: 'Şifre en az 3 karakter olmalıdır',
-            },
-            maxLength: {
-              value: 10,
-              message: 'Şifre en fazla 10 karakter olmalıdır',
-            },
-          }}
-        />
-      </View>
-      <CustomButton label="Onayla" onPress={handleSubmit(onSubmit)} />
-      <CustomButton label="Anasayfa" type="outlined" onPress={handleGoToHome} />
-    </View>
-  );
-};
-
-const SecondRoute = ({navigation}: DetailsProps) => {
-  const handleGoToHome = () => navigation.navigate('Home');
-  const {control, handleSubmit} = useForm<FormValues>();
-  const onSubmit: SubmitHandler<FormValues> = data => console.warn(data);
-  return (
-    <View style={styles.routeContainer}>
-      <CustomText
-        style={{
-          paddingVertical: PADDING.TEN,
-        }}
-        label={'Firs Screen is about to be implemented'}
-      />
-      <View style={styles.inputContainer}>
-        <CustomInput
-          label="Kullanıcı Adı"
-          control={control}
-          name="username"
-          placeholder="Kullanıcı Adı"
-          rules={{required: 'Kullanıcı Adı is required'}}
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <CustomInput
-          label="Şifre"
-          control={control}
-          name="password"
-          placeholder="Şifre"
-          secureTextEntry
-          rules={{
-            required: 'Şifre is required',
-            minLength: {
-              value: 3,
-              message: 'Şifre should be at least 3 characters',
-            },
-            maxLength: {
-              value: 10,
-              message: 'Şifre should be at most 10 characters',
-            },
-          }}
-        />
-      </View>
-      <CustomButton label="Submit" onPress={handleSubmit(onSubmit)} />
-      <CustomButton
-        label="Go to Home"
-        type="outlined"
-        onPress={handleGoToHome}
-      />
-    </View>
-  );
-};
-
-function DetailsScreen({...props}: DetailsProps) {
-  const layout = useWindowDimensions();
-  const [index, setIndex] = React.useState(0);
-
-  const renderScene = SceneMap({
-    first: () => <FirstRoute {...props} />,
-    second: () => <SecondRoute {...props} />,
-  });
-
-  const [routes] = React.useState([
-    {key: 'first', title: 'First'},
-    {key: 'second', title: 'Second'},
-  ]);
-
-  return (
-    <View style={styles.detailsContainer}>
-      <TabView
-        navigationState={{index, routes}}
-        renderScene={renderScene}
-        onIndexChange={setIndex}
-        renderTabBar={tabProps => (
-          <TabBar
-            {...tabProps}
-            indicatorStyle={{backgroundColor: colors.primary}}
-            style={styles.tabBar}
-            renderLabel={({route, focused}) => (
-              <CustomText
-                label={route.title}
-                style={{
-                  color: focused ? colors.primary : colors.gray,
-                  fontFamily: isAndroid ? FONT_FAMILY.REGULAR : undefined,
-                }}
-              />
-            )}
-          />
-        )}
-        initialLayout={{width: layout.width, height: layout.height}}
-        accessibilityLabel="TabView"
-      />
-    </View>
-  );
-}
+import {CalendarIcon, FavoriteIcon} from '../../assets/svg/Icons';
 
 const styles = StyleSheet.create({
   detailsContainer: {
@@ -187,5 +33,192 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
   },
 });
+
+const ScreenContainer = styled(View)`
+  background-color: ${colors.white};
+  flex: 1;
+`;
+
+const TabBarContainer = styled(TabBar)`
+  background-color: ${colors.white};
+  elevation: 0;
+`;
+
+const RouteContainer = styled(View)`
+  flex: 1;
+  padding: ${PADDING.FIFTEEN}px;
+`;
+
+const InputContainer = styled(View)`
+  padding: ${PADDING.TEN / 2}px 0px;
+`;
+
+const DetailsTitle = styled(CustomText)`
+  font-size: ${FONT_SIZE.LARGE}px;
+  text-align: right;
+  color: ${colors.scarlet};
+  padding-bottom: 10px;
+`;
+
+type DetailsProps = NativeStackScreenProps<RootStackParamList, 'Details'>;
+
+type FormValues = {
+  username: string;
+  password: string;
+};
+
+const FirstRoute = ({navigation}: DetailsProps) => {
+  const handleGoToHome = () => navigation.navigate('Home');
+  const {control, handleSubmit} = useForm<FormValues>({
+    context: {locale: 'en'},
+    criteriaMode: 'all',
+    defaultValues: {username: '', password: ''},
+    shouldFocusError: true,
+    delayError: 0,
+    mode: 'all',
+    reValidateMode: 'onChange',
+    resolver: undefined,
+    shouldUnregister: true,
+    shouldUseNativeValidation: false,
+  });
+  const onSubmit: SubmitHandler<FormValues> = formValues => {
+    console.warn(formValues);
+    handleGoToHome();
+  };
+
+  return (
+    <RouteContainer>
+      <CustomText label={'Firs Screen is about to be implemented'} />
+      <DetailsTitle
+        label={'Below is a form with react-hook-form and styled-components'}
+      />
+      <InputContainer>
+        <CustomInput
+          label="Kullanıcı Adı"
+          control={control}
+          name="username"
+          placeholder="Kullanıcı Adı"
+          rules={{required: 'Kullanıcı Adı zorunludur'}}
+        />
+      </InputContainer>
+      <InputContainer>
+        <CustomInput
+          leftIcon={iconProps => <CalendarIcon {...iconProps} />}
+          rightIcon={iconProps => <FavoriteIcon {...iconProps} />}
+          label="Şifre"
+          control={control}
+          name="password"
+          placeholder="Şifre"
+          rules={{
+            required: 'Şifre zorunludur',
+            minLength: {
+              value: 3,
+              message: 'Şifre en az 3 karakter olmalıdır',
+            },
+            maxLength: {
+              value: 10,
+              message: 'Şifre en fazla 10 karakter olmalıdır',
+            },
+          }}
+        />
+      </InputContainer>
+      <CustomButton label="Onayla" onPress={handleSubmit(onSubmit)} />
+      <CustomButton label="Anasayfa" type="outlined" onPress={handleGoToHome} />
+    </RouteContainer>
+  );
+};
+
+const SecondRoute = ({navigation}: DetailsProps) => {
+  const handleGoToHome = () => navigation.navigate('Home');
+  const {control, handleSubmit, watch, ...otherProps} = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = data => console.warn(data);
+  console.log(watch('username'), otherProps.formState.errors);
+
+  return (
+    <RouteContainer>
+      <CustomText
+        style={{
+          paddingVertical: PADDING.TEN,
+        }}
+        label={'Firs Screen is about to be implemented'}
+      />
+      <InputContainer>
+        <CustomInput
+          label="Kullanıcı Adı"
+          control={control}
+          name="username"
+          placeholder="Kullanıcı Adı"
+          rules={{required: 'Kullanıcı Adı is required'}}
+        />
+      </InputContainer>
+      <InputContainer>
+        <CustomInput
+          label="Şifre"
+          control={control}
+          name="password"
+          placeholder="Şifre"
+          secureTextEntry
+          rules={{
+            required: 'Şifre is required',
+            minLength: {
+              value: 3,
+              message: 'Şifre should be at least 3 characters',
+            },
+            maxLength: {
+              value: 10,
+              message: 'Şifre should be at most 10 characters',
+            },
+          }}
+        />
+      </InputContainer>
+      <CustomButton label="Submit" onPress={handleSubmit(onSubmit)} />
+      <CustomButton
+        label="Go to Home"
+        type="outlined"
+        onPress={handleGoToHome}
+      />
+    </RouteContainer>
+  );
+};
+
+function DetailsScreen({...props}: DetailsProps) {
+  const layout = useWindowDimensions();
+  const [index, setIndex] = React.useState(0);
+
+  const renderScene = SceneMap({
+    first: () => <FirstRoute {...props} />,
+    second: () => <SecondRoute {...props} />,
+  });
+
+  const [routes] = React.useState([
+    {key: 'first', title: 'First'},
+    {key: 'second', title: 'Second'},
+  ]);
+
+  return (
+    <ScreenContainer>
+      <TabView
+        navigationState={{index, routes}}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        renderTabBar={tabProps => (
+          <TabBarContainer
+            {...tabProps}
+            indicatorStyle={{backgroundColor: colors.primary}}
+            style={styles.tabBar}
+            renderLabel={({route, focused}) => (
+              <CustomText
+                label={route.title!}
+                style={{color: focused ? colors.primary : colors.gray}}
+              />
+            )}
+          />
+        )}
+        initialLayout={{width: layout.width, height: layout.height}}
+        accessibilityLabel="TabView"
+      />
+    </ScreenContainer>
+  );
+}
 
 export default DetailsScreen;
